44677c25a57fd0da608db1ce3aa5f4303bec4842
leandromoreira8325 <leandro.moreira@aliancaenergia.com.br>
2026-09-14 16:37:28 -0300

    Fix variable name to use correct spelling

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 import os
 
-# Parâmetros Oficiais da UHE Risoleta Neves (Candonga)
-AREA_OFFICIAL_HA = 1450.0
+AREA_OFICIAL_HA = 1450.0
 OUTPUT_DIR = os.path.join(os.getcwd(), "outputs")
 SHAPEFILE_NAME = "UHE_Risoleta_Neves_Reservatorio.shp"
