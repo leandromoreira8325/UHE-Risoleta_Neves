0bea5921c30f1bf5db9126d66d50d3f378304744
leandromoreira8325 <leandro.moreira@aliancaenergia.com.br>
2026-09-14 15:42:06 -0300

    Rename AREA_OFFICIAL_HA to AREA_OFICIAL_HA

--- src/config.py
+++ src/config.py
@@ -4,5 +4,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = BASE_DIR
 OUTPUT_DIR = os.path.join(BASE_DIR, "outputs")
 
-# Área oficial do reservatório em hectares
-AREA_OFFICIAL_HA = 1450.0
+# Área oficial do reservatório em hectares (em português)
+AREA_OFICIAL_HA = 1450.0
