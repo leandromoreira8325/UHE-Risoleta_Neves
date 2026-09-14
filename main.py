7164c999057eb81189e889014dd2648a28cb1497
leandromoreira8325 <leandro.moreira@aliancaenergia.com.br>
2026-09-14 08:48:32 -0300

    Add main.py file to project root
    Created main.py file in the root directory.

---
+++ main.py
@@ -0,0 +1,14 @@
+monitoramento_uhe_novo/
+│
+├── data/
+├── outputs/
+├── src/
+│   ├── __init__.py
+│   ├── config.py
+│   ├── copernicus_api.py
+│   ├── processing.py
+│   └── report.py
+│
+├── .gitignore
+├── main.py              <-- Criado bem aqui na raiz!
+└── requirements.txt
