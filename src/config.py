759cdce14873d6b725bf64f998643111331a96af
leandromoreira8325 <leandro.moreira@aliancaenergia.com.br>
2026-09-14 10:12:31 -0300

    Add configuration variables in config.py

---
+++ src/config.py
@@ -0,0 +1,6 @@
+import os
+
+UHE_NOME = "UHE Risoleta Neves (Candonga)"
+AREA_OFICIAL_AGUA_HA = 1450.0  
+SHAPEFILE_RESERVATORIO = os.path.join("data", "limite_reservatorio.geojson")
+OUTPUT_DIR = "output"
