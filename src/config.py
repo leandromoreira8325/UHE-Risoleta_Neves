fdc697dd25d12b389c3b7b9c693c5b4ca9117fcd
leandromoreira8325 <leandro.moreira@aliancaenergia.com.br>
2026-09-14 10:24:02 -0300

    Change reservoir shapefile to correct QLR file
    Updated shapefile path to point to the correct QLR file.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,9 @@
 import os
 
 UHE_NOME = "UHE Risoleta Neves (Candonga)"
+REGIAO_ATUACAO = "Alto/Médio Rio Doce (Rio Casca / Santa Cruz do Escalvado)"
 AREA_OFICIAL_AGUA_HA = 1450.0  
-SHAPEFILE_RESERVATORIO = os.path.join("data", "limite_reservatorio.geojson")
+
+# Apontando para o arquivo QLR correto localizado na pasta data
+SHAPEFILE_RESERVATORIO = os.path.join("data", "Resrevatório Candonga.qlr")
 OUTPUT_DIR = "output"
