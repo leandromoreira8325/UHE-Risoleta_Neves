efa8acb47119094b21658d09003628216ef8eaf9
leandromoreira8325 <leandro.moreira@aliancaenergia.com.br>
2026-09-15 13:44:22 -0300

    Update output directory and shapefile path in config

--- src/config.py
+++ src/config.py
@@ -1,5 +1,8 @@
 import os
 
+# Raiz do repositório (um nível acima da pasta src)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 AREA_OFICIAL_HA = 1450.0
-OUTPUT_DIR = os.path.join(os.getcwd(), "outputs")
-SHAPEFILE_NAME = "UHE_Risoleta_Neves_Reservatorio.shp"
+OUTPUT_DIR = os.path.join(BASE_DIR, "outputs")
+SHAPEFILE_PATH = os.path.join(BASE_DIR, "UHE_Risoleta_Neves_Reservatorio.shp")
