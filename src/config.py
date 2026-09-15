3bddb3938bfaeb68695f1ddd0da353eaa1b4b1f4
leandromoreira8325 <leandro.moreira@aliancaenergia.com.br>
2026-09-15 14:09:14 -0300

    Refactor paths in config.py to use pathlib

--- src/config.py
+++ src/config.py
@@ -1,11 +1,9 @@
-import os
-from pathlib import Path
+ifrom pathlib import Path
 
-# Localiza a raiz do repositório (um nível acima de src/)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 AREA_OFICIAL_HA = 1450.0
-OUTPUT_DIR = os.path.join(BASE_DIR, "outputs")
 
-# Caminho absoluto para o Shapefile na raiz
-SHAPEFILE_PATH = os.path.join(BASE_DIR, "UHE_Risoleta_Neves_Reservatorio.shp")
+OUTPUT_DIR = BASE_DIR / "outputs"
+
+SHAPEFILE_PATH = BASE_DIR / "UHE_Risoleta_Neves_Reservatorio.shp"
